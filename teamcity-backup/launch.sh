762327ba5da1dd43797cdca523cde4279837105a
Kristian Martensen <km@shipbeat.com>
2016-05-04 15:28:10 +0200

    renamed launch to launch.sh

--- teamcity-backup/launch.sh
+++ teamcity-backup/launch.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 python3 /teamcity_backup.py -base_url $TEAMCITY_BASE_URL -username $TEAMCITY_PASSWORD -password $TEAMCITY_PASSWORD
 python3 /push_to_s3.py -bucket $AWS_S3_BUCKET -access-key $AWS_ACCESS_KEY -secret-key $AWS_SECRET_KEY -threshold $KEEP_DAYS
 rm -f /var/lib/teamcity/backup/*.zip
