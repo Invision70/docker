089624d8f5c757d1f08a13432d13702630e8abd0
Kristian Martensen <km@shipbeat.com>
2016-05-04 16:03:58 +0200

    fixed teamcity credential bummer

--- teamcity-backup/launch.sh
+++ teamcity-backup/launch.sh
@@ -1,3 +1,3 @@
-python3 /teamcity_backup.py -base_url $TEAMCITY_BASE_URL -username $TEAMCITY_PASSWORD -password $TEAMCITY_PASSWORD
+python3 /teamcity_backup.py -base_url $TEAMCITY_BASE_URL -username $TEAMCITY_USERNAME -password $TEAMCITY_PASSWORD
 python3 /push_to_s3.py -bucket $AWS_S3_BUCKET -access-key $AWS_ACCESS_KEY -secret-key $AWS_SECRET_KEY -threshold $KEEP_DAYS
 rm -f /var/lib/teamcity/backup/*.zip
